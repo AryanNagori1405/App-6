51d64f78f84dc5f7d9ce0fc63e4ca2c9a39136f2
ARYAN <aryannikill123@gmail.com>
2025-01-19 00:56:29 +0530

    Add the content of text file onto PDF file.

--- main.py
+++ main.py
@@ -2,24 +2,34 @@ from fpdf import FPDF
 import glob
 from pathlib import Path
 
+# Create a list of text filepaths
 filepaths = glob.glob("text files/*.txt")
 
+# Create a PDF file
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
+# Iterate over each of the text file
 for filepath in filepaths:
+    # Add a page to the PDF document for each text file
     pdf.add_page()
 
+    # Get the filename without the extension
     file_path = Path(filepath).stem
+    # Convert it to title case(e.g. Cat)
     filename = file_path.title()
 
+    # Add the name of the PDF
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=filename, ln=1)
-    pdf.line(10, 20, 200, 20)
+    # Add a line below the name
+    pdf.line(10, 18, 200, 18)
 
-    with open(filename) as file:
-        file_info = file.read()
-
+    # Get the content of the text file
+    with open(filepath) as file:
+        content = file.read()
+    # Add the text file content to PDF
     pdf.set_font(family="Times", size=12)
-    pdf.cell(w=15, h=15, txt=file_info, ln=True)
+    pdf.multi_cell(w=0, h=10, txt=content)
 
+# Produce the PDF
 pdf.output("output.pdf")
